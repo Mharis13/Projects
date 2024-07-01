6be95971e104e70dc48135bbd1e3a4585a2493f8
Mharis13 <137278432+Mharis13@users.noreply.github.com>
2024-07-01 23:38:37 +0200

    LOL_API

--- 2024/Python/Rest_API_LOL/src/config/db.py
+++ 2024/Python/Rest_API_LOL/src/config/db.py
@@ -1,10 +1,11 @@
-from sqlalchemy import create_engine,MetaData
+from sqlalchemy import create_engine, MetaData
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 db_password = os.getenv("DB_PASSWORD")
-engine = create_engine("mysql+pymysql://root:{db_password}@localhost:3306/leagueoflegendsapi")
+
+engine = create_engine(f"mysql+pymysql://root:{db_password}@localhost:3306/leagueoflegendsapi")
 meta = MetaData()
 conn = engine.connect()
